246cfb1380cdb60e0cbb9eed598986dfc7173d6e
LLiRRiK <kill2002@mail.ru>
2023-08-02 09:51:11 +0300

    revision of function

--- src/Operations_on_accounts.py
+++ src/Operations_on_accounts.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 
 def read_json():
@@ -13,7 +14,7 @@ def mask_card_number(card_number):
 
 
 def mask_account_number(account_number):
-    masked_number = "**" + account_number[-4:]
+    masked_number = account_number[:4] + " **" + account_number[-4:]
     return masked_number
 
 
@@ -21,12 +22,13 @@ def print_last_5_operations(operations):
     executed_transactions = [transaction for transaction in operations if transaction.get("state", '') == 'EXECUTED']
     operations = sorted(executed_transactions, key=lambda x: x['date'], reverse=True)[:5]
     for operation in operations:
-        date = operation['date'][:10]
+        date_ = time.strptime(operation['date'][:10], "%Y-%m-%d")
+        date = time.strftime("%d.%m.%Y", date_)
         description = operation['description']
         from_account = mask_card_number(operation.get('from', ''))
         to_account = mask_account_number(operation.get('to', ''))
         amount = operation['operationAmount']['amount']
-        currency = operation['operationAmount']['currency']['code']
+        currency = operation['operationAmount']['currency']['name']
 
         print(f"{date} {description}")
         print(f"{from_account} -> {to_account}")
